ac9eb9d511ea170d3b2dcd9257e9371c3af418ee
guilherme-hl1ma <guihlima@outlook.com>
2024-07-16 11:50:48 -0300

    add prod site url

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://rocky-river-33142-c43aadc9cb7e.herokuapp.com/';
 export const POSTS_URL = `${API_URL}api/postss?populate=*`;
 
 export const SITE_NAME = 'Guilherme Lima';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://snazzy-rugelach-022de6.netlify.app';
